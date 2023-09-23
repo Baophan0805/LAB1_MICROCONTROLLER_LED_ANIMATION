a40cb371163f419a92ef5d2c9443209b3751aac6
BAOPHAN <phangiabao9999@gmail.com>
2023-09-23 17:48:27 +0700

    Complete Ex3

--- LAB1_Exercise/Core/Inc/main.h
+++ LAB1_Exercise/Core/Inc/main.h
@@ -58,12 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
-#define LED_GREEN_Pin GPIO_PIN_7
-#define LED_GREEN_GPIO_Port GPIOA
+#define GPIO_Output_Pin GPIO_PIN_5
+#define GPIO_Output_GPIO_Port GPIOA
+#define GPIO_OutputA6_Pin GPIO_PIN_6
+#define GPIO_OutputA6_GPIO_Port GPIOA
+#define GPIO_OutputA7_Pin GPIO_PIN_7
+#define GPIO_OutputA7_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
